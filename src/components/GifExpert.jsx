43bba8c72dcb06db6dcea4945668507d0a5eaeb4
jonathanbello123 <jonathanbellogabriel@gmail.com>
2024-05-03 12:09:05 -0300

    second commit

--- src/components/GifExpert.jsx
+++ src/components/GifExpert.jsx
@@ -11,20 +11,17 @@ export const GifExpert = () => {
 
     const { handleCheked, isChecked } = useContext(GifContext)
 
-
     return (
-        <div>
-            <div className='flex flex-col justify-center items-center gap-8 '>
-                <div className='flex'>
-                    <h1 className={`${isChecked ? "text-black" : "text-black"} text-white text-4xl text-center mt-10`}>
-                        GIF GALAXY
-                    </h1>
-                    <div onClick={handleCheked} className="absolute mr-[-40px]  mt-8 right-10" >
-                        <CustomizedSwitches />
-                    </div>
+        <div className='flex flex-col justify-center items-center gap-8'>
+            <div className='flex justify-between items-center w-full'>
+                <h1 className={`text-${isChecked ? 'black' : 'black'} text-white text-4xl text-center m-auto mt-12`}>
+                    GIF GALAXY
+                </h1>
+                <div className='mt-12' onClick={handleCheked}>
+                    <CustomizedSwitches />
                 </div>
-                <FormInput />
             </div>
-        </div >
+            <FormInput />
+        </div>
     )
 }
